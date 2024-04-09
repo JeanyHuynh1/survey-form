c869c79a0a545cfc26d48201649de98d45d97332
JeanyHuynh1 <jeanyhuynhtn@gmail.com>
2024-04-09 22:57:13 +0500

    feat: updated api urls on the front end side

--- client/src/api/userApi.jsx
+++ client/src/api/userApi.jsx
@@ -3,11 +3,11 @@ import axios from 'axios'
 export class UserServices {
     static async registerUser(userData) {
         //will return jwt token
-       return await axios.post('http://localhost:8080/api/users/register',userData);
+       return await axios.post('https://survey-form-api.onrender.com/api/users/register',userData);
     }
     static async loginUser (loginData) {
         try {
-            const response = await axios.post('http://localhost:8080/api/users/login', loginData);
+            const response = await axios.post('https://survey-form-api.onrender.com/api/users/login', loginData);
             return response.data;
         }
         catch {
@@ -18,7 +18,7 @@ export class UserServices {
         //will return jwt token
         const userToken = localStorage.getItem('userToken');
 
-       return await axios.get(`http://localhost:8080/api/users/current`, {
+       return await axios.get(`https://survey-form-api.onrender.com/api/users/current`, {
             headers: {
                Authorization: userToken ? `Bearer ${userToken}` : undefined,
            }
@@ -30,7 +30,7 @@ export class SurveyServices {
     static async fetchAllSurveys() {
         const userToken = localStorage.getItem('userToken');
         
-        return await axios.get(`http://localhost:8080/api/surveys`, {
+        return await axios.get(`https://survey-form-api.onrender.com/api/surveys`, {
             headers: {
                Authorization: userToken ? `Bearer ${userToken}` : undefined,
            }
@@ -40,7 +40,7 @@ export class SurveyServices {
     static async CreateSurvey(surveyData) {
         const userToken = localStorage.getItem('userToken');
 
-        return await axios.post(`http://localhost:8080/api/surveys`, surveyData, {
+        return await axios.post(`https://survey-form-api.onrender.com/api/surveys`, surveyData, {
             headers: {
                 Authorization: userToken ? `Bearer ${userToken}` : '',
             }
@@ -50,7 +50,7 @@ export class SurveyServices {
     static async updateSurvey(surveyID, surveyData) {
         const userToken = localStorage.getItem('userToken');
 
-        return await axios.put(`http://localhost:8080/api/surveys/${surveyID}`, surveyData, {
+        return await axios.put(`https://survey-form-api.onrender.com/api/surveys/${surveyID}`, surveyData, {
             headers: {
                 Authorization: userToken ? `Bearer ${userToken}` : '',
             }
@@ -60,7 +60,7 @@ export class SurveyServices {
     static async deleteSurvey(surveyID) {
         const userToken = localStorage.getItem('userToken');
         
-        return await axios.delete(`http://localhost:8080/api/surveys/${surveyID}`, {
+        return await axios.delete(`https://survey-form-api.onrender.com/api/surveys/${surveyID}`, {
             headers: {
                 Authorization: userToken ? `Bearer ${userToken}` : '',
             }
